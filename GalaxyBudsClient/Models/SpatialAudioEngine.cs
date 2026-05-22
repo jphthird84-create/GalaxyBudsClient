3c89b51d046df8ca46e2c43e3792a02029749f64
jphthird84-create <jphthird84@gmail.com>
2026-05-22 01:33:28 -0500

    feat: integrate SOFA HRTF loader

--- GalaxyBudsClient/Models/SpatialAudioEngine.cs
+++ GalaxyBudsClient/Models/SpatialAudioEngine.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Numerics;
 using GalaxyBudsClient.Models;
+using GalaxyBudsClient.Models.SOFA;
 
 namespace GalaxyBudsClient.Models;
 
 public class SpatialAudioEngine
 {
-    private SpatialQuaternion _currentHeadOrientation = new SpatialQuaternion(0, 0, 0, 1);
-    private readonly object _lock = new object();
+    private SpatialQuaternion _currentHeadOrientation = new(0, 0, 0, 1);
+    private readonly HRTFProcessor _hrtfProcessor = new();
+    private readonly object _lock = new();
 
     public event EventHandler<SpatialQuaternion>? HeadOrientationChanged;
 
@@ -16,25 +18,31 @@ public class SpatialAudioEngine
         lock (_lock)
         {
             _currentHeadOrientation = quaternion;
+            var euler = QuaternionToEuler(quaternion);
+            _hrtfProcessor.UpdateHRTF(euler.Yaw, euler.Pitch);
             HeadOrientationChanged?.Invoke(this, quaternion);
         }
     }
 
+    private (float Yaw, float Pitch) QuaternionToEuler(SpatialQuaternion q)
+    {
+        float yaw = (float)(Math.Atan2(2 * (q.W * q.Y + q.X * q.Z), 1 - 2 * (q.Y * q.Y + q.Z * q.Z)) * 180 / Math.PI);
+        float pitch = (float)(Math.Asin(2 * (q.W * q.X - q.Z * q.Y)) * 180 / Math.PI);
+        return (yaw, pitch);
+    }
+
+    public (float left, float right) ProcessAudioSample(float monoSample)
+    {
+        return _hrtfProcessor.ProcessSample(monoSample);
+    }
+
+    public bool LoadHRTF(string sofaFilePath)
+    {
+        return HRTFLoader.LoadSOFA(sofaFilePath, out var left, out var right);
+    }
+
     public SpatialQuaternion GetCurrentOrientation()
     {
-        lock (_lock)
-        {
-            return _currentHeadOrientation;
-        }
+        lock (_lock) return _currentHeadOrientation;
     }
-
-    // Simple rotation matrix for audio spatialization (can be extended with HRTF later)
-    public Vector3 RotateAudioSource(Vector3 sourcePosition)
-    {
-        var q = GetCurrentOrientation();
-        var rotation = Quaternion.CreateFromRotationMatrix(Matrix4x4.CreateFromQuaternion(new Quaternion(q.X, q.Y, q.Z, q.W)));
-        return Vector3.Transform(sourcePosition, rotation);
-    }
-
-    public override string ToString() => $"SpatialAudioEngine - Current: {_currentHeadOrientation}";
 }
